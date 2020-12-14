89f59b5ac1ae780b74e95d09729c5c540376e91e
layla hmidene <layla.hmidene@etudiant-fst.utm.tn>
2020-12-14 13:17:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var rect = {
+    perimetre: (x,y) =>  (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+function solveRect(l,b) {
+    console.log ("solving for rectangle with l=" +l + "and b=" +b);
+
+    if (l <= 0 || b <= 0){
+        console.log("rectangle dimensions should be greater than zero: l=" +l + "and b=" +b);
+    }
+    else{ 
+        console.log("the area of the rectangle is"+ rect.area(l,b));
+        console.log("the perimetre of the rectangle is"+ rect.perimetre(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(5,6);
